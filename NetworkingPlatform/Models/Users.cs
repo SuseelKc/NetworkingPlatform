5cea215b2f3d1fe76ff6a1467e042ccdf2f84223
SuseelKc <sushil.np.khatri@gmail.com>
2024-05-08 14:34:22 +0545

    users properties

--- NetworkingPlatform/Models/Users.cs
+++ NetworkingPlatform/Models/Users.cs
@@ -3,19 +3,23 @@ using System.ComponentModel.DataAnnotations;
 
 namespace NetworkingPlatform.Models
 {
-   
 
-        public class Users:IdentityUser
-        {
-            //[Key]
-            //public int ID { get; set; }
-            //public string Name { get; set; }
-            //public string ContactNo { get; set; }
-            //public string Email { get; set; }
-            //public string Password { get; set; }
-            //public UserRole userRole { get; set; }
 
-        }
+    public class Users : IdentityUser
+    {
+        //[Key]
+        //public int ID { get; set; }
+        //public string Name { get; set; }
+        //public string ContactNo { get; set; }
+        //public string Email { get; set; }
+        //public string Password { get; set; }
+        //public UserRole userRole { get; set; }
 
-    
+        public string? Image { get; set; }
+        [Required]
+        public int UserRole { get; set; }
+
+    }
+
+
 }
